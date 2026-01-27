2fb2e6ead63251a78d2e4bd9a22bf25b03a02af8
happyd1624 <happyd1624@gmail.com>
2026-01-27 22:53:19 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,3 +1,5 @@
+#task1
+
 def read_sample_file(filename):
     try:
         with open(filename, 'r') as file:
@@ -10,3 +12,19 @@ def read_sample_file(filename):
         print(f"An unexpected error occurred: {e}")
 
 read_sample_file("sample.txt")
+
+
+#task2
+
+
+user_data = input("Enter data to write: ")
+with open("output.txt", "w") as file:
+    file.write(user_data + "\n")
+
+additional_data = input("Enter data to append: ")
+with open("output.txt", "a") as file:
+    file.write(additional_data + "\n")
+
+print("\nFinal Content of output.txt:")
+with open("output.txt", "r") as file:
+    print(file.read())
